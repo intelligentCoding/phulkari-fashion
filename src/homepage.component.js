163de4e275ddb2adf77a80be42e34403cc7c55fd
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-03-26 19:15:35 -0700

    sass setup done

---
+++ src/homepage.component.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Phulkari Pants</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Dupata</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hand Embroidered</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Traditional Jagga</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Punjabi Phulkari Suit</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Embroidered Lawn Suit</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Traditional Phulkari</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Modern Phulkari</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
