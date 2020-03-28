6edf4d4d99acf45cbbac04580018e8f8508ff55e
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-03-28 14:39:12 -0700

    installed router and update menu-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+const PhulkariPants = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+      <Route  exact path='/' component={HomePage}></Route>
+      <Route  path='/phulkari-pants' component={PhulkariPants}></Route>
+      </Switch>
     </div>
+    // <div>
+    //   <HomePage></HomePage>
+    // </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,51 +12,51 @@ class Directory extends React.Component{
               title: 'Phulkari Pants',
               imageUrl: 'https://i.etsystatic.com/20015124/r/il/455899/1849552192/il_794xN.1849552192_jjgb.jpg',
             //   size: 'large',
-              id: 4,
-              linkUrl: 'shop/womens'
+              id: 1,
+              linkUrl: 'phulkari-pants'
             },
             {
               title: 'Dupatta',
               imageUrl: 'https://i.pinimg.com/564x/19/fe/7d/19fe7d0bdbd90731c53b56e738f64fe5.jpg',
             //   size: 'large',
-              id: 5,
+              id: 2,
               linkUrl: 'shop/mens'
             },
             {
                 title: 'Bridal Phulkari',
                 imageUrl: 'https://i.pinimg.com/564x/e1/5d/51/e15d51ad899f8d360a84cca6933b0aba.jpg',
-                id: 1,
+                id: 3,
                 linkUrl: 'shop/hats'
               },
               {
                 title: 'Traditional Phulkari',
                 imageUrl: 'https://i.pinimg.com/564x/69/18/48/691848eb20968a96cf57ce7246ba8661.jpg',
-                id: 2,
+                id: 4,
                 linkUrl: 'shop/jackets'
               },
               {
                 title: 'Modern Phulkari',
                 imageUrl: 'https://i.pinimg.com/564x/79/fa/c5/79fac5d276e5b59d915e424b6cae2e86.jpg',
-                id: 3,
+                id: 5,
                 linkUrl: 'shop/sneakers'
               },
               {
                 title: 'Hand Embroidred',
                 imageUrl: 'https://i.pinimg.com/564x/70/38/40/703840c9782f4623732bcfc6a9a9f45d.jpg',
-                id: 1,
+                id: 6,
                 linkUrl: 'shop/hats'
               },
               {
                 title: 'Traditional Jagga',
                 imageUrl: 'https://i.pinimg.com/564x/79/fa/c5/79fac5d276e5b59d915e424b6cae2e86.jpg',
-                id: 2,
+                id: 7,
                 size: 'large',
                  linkUrl: 'shop/jackets'
               },
               {
                 title: 'Lehenga Choli',
                 imageUrl: 'https://i.pinimg.com/564x/e1/5d/51/e15d51ad899f8d360a84cca6933b0aba.jpg',
-                id: 3,
+                id: 8,
                 size: 'large',
                 linkUrl: 'shop/sneakers'
               }]
@@ -67,8 +67,8 @@ class Directory extends React.Component{
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem size={size} imageUrl={imageUrl} key={id} title={title}></MenuItem>
+                    this.state.sections.map(({id, ...otherProps}) => (
+                        <MenuItem key={id} {...otherProps}></MenuItem>
                     ))
                 }
             </div> 
