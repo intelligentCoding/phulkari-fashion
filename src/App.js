952bf99668d3c526442ed971d01b21aa01a808a6
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-03-29 19:35:19 -0700

    added signin component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SigninAndSingup from './pages/signin-signup/signin-signup-component';
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Switch>
       <Route  exact path='/' component={HomePage}></Route>
       <Route  path='/shop' component={ShopPage}></Route>
+      <Route  path='/signin' component={SigninAndSingup}></Route>
       </Switch>
     </div>
     // <div>
